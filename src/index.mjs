1e05e3cdf3c823e8a60c7b8a37ddaa9e3f84af58
Alexandre Wagane NDIAYE <wagane1996@gmail.com>
2024-09-14 15:01:10 +0200

    Make Get, Post request

---
+++ src/index.mjs
@@ -0,0 +1,63 @@
+import express, { urlencoded } from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app= express();
+const port =process.env.PORT;
+const mockUsers =[
+    {id:1,userName:"Anson", displayName:"Anson"},
+    {id:2,userName:"John", displayName:"John"},
+    {id:3,userName:"Ellie", displayName:"Ellie"}
+]
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/",(req,res)=>{
+    res.status(201).send({"msg":"Hello"})
+})
+
+app.get("/api/users",(req,res)=>{
+    const {filter,value} = req.query
+    console.log(req.query);
+    if(!filter || !value) return res.send(mockUsers) 
+    const filteredUsers = mockUsers.filter((user)=>user[filter].includes(value))
+    console.log(filteredUsers);
+    console.log(mockUsers);
+    if(!filteredUsers) return res.send(mockUsers) 
+    return res.send(filteredUsers)
+    
+})
+
+app.get("/api/users/:id",(req,res)=>{
+    const {id}=req.params
+
+    const user = mockUsers.find(user=>user.id==id)
+    if(!user) return res.sendStatus(404)
+    return res.send(user)
+    
+})
+
+app.post('/api/users',(req,res)=>{
+    const {body}=req;
+    if(!body) return sendStatus(404)
+    const newUser = {id:mockUsers.at(-1).id+1,...body}
+    mockUsers.push(newUser)
+    // console.log('Hee ',newUser);
+    res.status(201).send(newUser)
+    
+})
+
+app.get("/api/products",(req,res)=>{
+    res.send([
+        {id:123,Name:"Chicken George", price:1.99},
+        
+    ])
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Serveur démarré sur le port ${port}`);
+  });
